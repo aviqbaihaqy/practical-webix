1f45f79c7ce0647498b3a21f4bb4813f5680e65e
Aviq Baihaqy <aviq.baihaqy@gmail.com>
2018-10-06 08:53:26 +0700

    initial commit

---
+++ sources/app.js
@@ -0,0 +1,22 @@
+import "./styles/app.css";
+import {JetApp, EmptyRouter, HashRouter } from "webix-jet";
+
+export default class MyApp extends JetApp{
+	constructor(config){
+		const defaults = {
+			id 		: APPNAME,
+			version : VERSION,
+			router 	: BUILD_AS_MODULE ? EmptyRouter : HashRouter,
+			debug 	: !PRODUCTION,
+			start 	: "/top/start"
+		};
+
+		super({ ...defaults, ...config });
+
+		/*wjet::plugin*/
+	}
+}
+
+if (!BUILD_AS_MODULE){
+	webix.ready(() => new MyApp().render() );
+}
